67cb9aba7d1c4a89d01946c5d024092fbbc24daa
leandro dolensi <leandro.dolensi@aluno.ifsp.edu.br>
2023-09-24 15:18:55 -0300

    test: finish first equivalent class for robot rotation
    all tests pass in first execution, no need feat or fix in mr robot class

--- src/test/java/MrRobotTest.java
+++ src/test/java/MrRobotTest.java
@@ -49,4 +49,76 @@ public class MrRobotTest {
 
         assertThat(result).isEqualTo(expected);
     }
+
+    @ParameterizedTest
+    @CsvSource({"0, 4, (-4 0)",
+                "45, 6, (-4 4)",
+                "90, 4, (0 4)",
+                "135, 6, (4 4)",
+                "180, 4, (4 0)"})
+    @DisplayName("should return valid obstacle position when robot angle is 90 and position is zero")
+    void shouldReturnValidObstaclePositionWhenRobotAngleIs90AndPositionIsZero(
+            int feixeValue, int feixeDistance, String expected
+    ){
+        sut.setRobotPosition(0, 0, 90);
+        sut.setFeixe(feixeValue, feixeDistance);
+
+        String result = sut.getObstaclePosition();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 6, (-4 -4)",
+                "45, 4, (-4 0)",
+                "90, 6, (-4 4)",
+                "135, 4, (0 4)",
+                "180, 6, (4 4)"})
+    @DisplayName("should return valid obstacle position when robot angle is 135 and position is zero")
+    void shouldReturnValidObstaclePositionWhenRobotAngleIs135AndPositionIsZero(
+            int feixeValue, int feixeDistance, String expected
+    ){
+        sut.setRobotPosition(0, 0, 135);
+        sut.setFeixe(feixeValue, feixeDistance);
+
+        String result = sut.getObstaclePosition();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 4, (0 -4)",
+                "45, 6, (-4 -4)",
+                "90, 4, (-4 0)",
+                "135, 6, (-4 4)",
+                "180, 4, (0 4)"})
+    @DisplayName("should return valid obstacle position when robot angle is 180 and position is zero")
+    void shouldReturnValidObstaclePositionWhenRobotAngleIs180AndPositionIsZero(
+            int feixeValue, int feixeDistance, String expected
+    ){
+        sut.setRobotPosition(0, 0, 180);
+        sut.setFeixe(feixeValue, feixeDistance);
+
+        String result = sut.getObstaclePosition();
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0, 4, (4 0)",
+                "45, 6, (4 -4)",
+                "90, 4, (0 -4)",
+                "135, 6, (-4 -4)",
+                "180, 4, (-4 0)"})
+    @DisplayName("should return valid obstacle position when robot angle is 270 and position is zero")
+    void shouldReturnValidObstaclePositionWhenRobotAngleIs270AndPositionIsZero(
+            int feixeValue, int feixeDistance, String expected
+    ){
+        sut.setRobotPosition(0, 0, 270);
+        sut.setFeixe(feixeValue, feixeDistance);
+
+        String result = sut.getObstaclePosition();
+
+        assertThat(result).isEqualTo(expected);
+    }
 }
